6400ebab1c83125de75c93d67a4d3fa10974a5be
91707 <sreedhar70755@gmail.com>
2024-01-02 14:41:06 +0530

    k8s

--- cluster-k8s-kops/three-master-cluster.sh
+++ cluster-k8s-kops/three-master-cluster.sh
@@ -3,4 +3,4 @@ kops create cluster --name= cloudev7.online \
 --node-count=3 --control-plane-count=3 --node-size=t3.medium --control-plane-size=t3.medium \
 --control-plane-zones=us-east-1a,us-east-1b,us-east-1c --control-plane-volume-size 10 --node-volume-size 10 \
 --ssh-public-key ~/.ssh/id_rsa.pub \
---dns-zone=cloudev7.online  --dry-run --output yaml
+--dns-zone=cloudev7.online  --yes # --dry-run --output yaml
